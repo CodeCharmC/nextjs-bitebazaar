8e4dfef291f7625c27c4bbfbac738e756f81faae
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-06 21:48:26 +0600

    create BBLogo component

---
+++ app/ui/bb-logo.tsx
@@ -0,0 +1,11 @@
+
+
+export default function BBLogo() {
+   return (
+      <div className="absolute top-6 left-8">
+         <h1 className="text-4xl font-extrabold text-orange-600 drop-shadow-md">
+           Bite<span className="text-purple-600">Bazaar</span>
+         </h1>
+      </div>
+   )
+}
