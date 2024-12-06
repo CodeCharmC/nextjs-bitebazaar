d4e3e6fea401b7f15c7f091f140faf57f474b7ac
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-06 21:47:51 +0600

    make adjustments to tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,18 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        text: 'text 3s ease infinite',
+      },
       gridTemplateColumns: {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
-      colors: {
-        blue: {
-          400: '#2589FE',
-          500: '#0070F3',
-          600: '#2F6FEB',
-        },
-      },
     },
     keyframes: {
+      text: {
+        '0%, 100%': { backgroundPosition: '0% 50%' },
+        '50%': { backgroundPosition: '100% 50%' },
+      },
       shimmer: {
         '100%': {
           transform: 'translateX(100%)',
