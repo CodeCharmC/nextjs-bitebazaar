71fb09fc30a7dc066639948ef8bb2a2f0a14b659
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-07 22:16:00 +0600

    modify BBLogo

--- app/ui/bb-logo.tsx
+++ app/ui/bb-logo.tsx
@@ -2,8 +2,8 @@
 
 export default function BBLogo() {
    return (
-      <div className="absolute top-6 left-8">
-         <h1 className="text-4xl font-extrabold text-orange-600 drop-shadow-md">
+      <div className="absolute top-4 left-5 md:top-6 md:left-8">
+         <h1 className="text-3xl md:text-4xl font-extrabold text-orange-600 drop-shadow-md">
            Bite<span className="text-purple-600">Bazaar</span>
          </h1>
       </div>
