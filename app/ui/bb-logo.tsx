0db0ed0dfc7ca8a7752f52c9c56894b5799b8275
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-07 23:02:11 +0600

    small correction

--- app/ui/bb-logo.tsx
+++ app/ui/bb-logo.tsx
@@ -2,7 +2,7 @@
 
 export default function BBLogo() {
    return (
-      <div className="absolute top-4 left-5 md:top-6 md:left-8">
+      <div className="top-8 left-5 md:top-8 md:left-8">
          <h1 className="text-3xl md:text-4xl font-extrabold text-orange-600 drop-shadow-md">
            Bite<span className="text-purple-600">Bazaar</span>
          </h1>
