e1cc0b1f40fc37b2ee30c6097922efd80f7fe91d
CodeCharmC <moni.akhtar1066@gmail.com>
2024-12-08 22:50:14 +0600

    add link to logo

--- app/ui/bb-logo.tsx
+++ app/ui/bb-logo.tsx
@@ -1,10 +1,10 @@
-
+import Link from "next/link";
 
 export default function BBLogo() {
    return (
       <div className="top-8 left-5 md:top-8 md:left-8">
          <h1 className="text-3xl md:text-4xl font-extrabold text-orange-600 drop-shadow-md">
-           Bite<span className="text-purple-600">Bazaar</span>
+            <Link href="/dashboard">Bite<span className="text-purple-600">Bazaar</span></Link>           
          </h1>
       </div>
    )
